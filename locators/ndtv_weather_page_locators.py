c8c39d236e5c23b1fab5d28d1c6b0b73991f8e0c
Mandeep Singh <manumandeep1818@gmail.com>
2021-04-19 10:31:10 +0530

    Added ndtv weather locators

---
+++ locators/ndtv_weather_page_locators.py
@@ -0,0 +1,5 @@
+class NdtvWeatherPageLocators:
+    search_box = "id,searchBox"
+    city = "id,Chandigarh"
+    city_weather_info_on_map = "css,div[title=\"Chandigarh\"]"
+    city_weather_temp_on_map = "css,div[title=\"Chandigarh\"] .tempRedText"
