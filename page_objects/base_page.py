7281df0221ca4ae96b86a629b3b5fe41a8c28b41
Mandeep Singh <manumandeep1818@gmail.com>
2021-04-19 10:29:40 +0530

    Added split locator, wait methods

--- page_objects/base_page.py
+++ page_objects/base_page.py
@@ -1,12 +1,43 @@
+import unittest
+
+from selenium.webdriver.support.wait import WebDriverWait
+
 import testdata.urls as url
 from page_objects.driver_factory import DriverFactory
+from selenium.webdriver.support import expected_conditions as EC
 
 
-class BasePage:
+def write(msg):
+    print(msg)
+
+
+class Borg:
+    # The borg design pattern is to share state
+    # Src: http://code.activestate.com/recipes/66531/
+    __shared_state = {}
+
     def __init__(self):
+        self.__dict__ = self.__shared_state
+
+    def is_first_time(self):
+        """ Has the child class been invoked before? """
+        result_flag = False
+        if len(self.__dict__) == 0:
+            result_flag = True
+        return result_flag
+
+
+class BasePage(Borg, unittest.TestCase):
+    def __init__(self):
+        """ Constructor """
+        Borg.__init__(self)
+        if self.is_first_time():
+            self.driver = None
+            self.result_flag = False
         self.base_url = url.ndtv_url
         self.driver_obj = DriverFactory()
-        self.launch_page()
+        if self.driver is not None:
+            self.launch_page()
 
     def launch_page(self):
         """Overwrite this method in your Page module if you want to visit a specific URL"""
@@ -27,4 +58,39 @@ class BasePage:
         :param url:
         :return:
         """
-        self.driver.get(self.base_url+url)
+        self.driver.get(self.base_url + url)
+
+    def split_locator(self, locator):
+        """ Split the locator type and locator """
+        try:
+            loc = locator.split(',')
+            if loc[0] == "css":
+                loc[0] = "css selector"
+            return tuple([loc[0], loc[1]])
+
+        except Exception as e:
+            write("Error while parsing locator")
+            raise e
+
+    def get_element(self, locator, timeout=20):
+        """
+
+        :param locator:
+        :param timeout:
+        :param retry_interval:
+        :return:
+        """
+        dom_element = None
+        path = self.split_locator(locator)
+        try:
+            self.smart_wait(timeout, locator)
+            dom_element = self.driver.find_element(*path)
+            self.smart_wait(timeout, locator)
+            return dom_element
+        except Exception as e:
+            write("Error While getting locator")
+            raise e
+
+    def smart_wait(self, wait_seconds, locator):
+        path = self.split_locator(locator)
+        WebDriverWait(self.driver, wait_seconds).until(EC.presence_of_element_located(path))
