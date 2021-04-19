c6a2175d6d5601c14927e3ead422003f6214ad60
Mandeep Singh <manumandeep1818@gmail.com>
2021-04-19 10:28:28 +0530

    Added new locators

--- locators/ndtv_main_page_locators.py
+++ locators/ndtv_main_page_locators.py
@@ -1,4 +1,4 @@
 class NdtvMainPageLocators:
-    overflow_button_nav_bar = ""
-    weather_button_nav_bar = ""
+    overflow_button_nav_bar = "id,h_sub_menu"
+    weather_button_nav_bar = "xpath,//a[text() = 'WEATHER']"
     no_thanks_button = "css selector,a[class=\"notnow\"]"
